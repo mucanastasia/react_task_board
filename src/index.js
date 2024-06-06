d3514cdfe8de2d454dd50fafa921b43dc6634fe0
mucanastasia <44086697+mucanastasia@users.noreply.github.com>
2024-06-06 21:17:59 +0200

    Adding basename for router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/react_task_board'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
