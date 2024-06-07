969e45517d9733e56b99b15f47dcd7765890f6b5
mucanastasia <44086697+mucanastasia@users.noreply.github.com>
2024-06-07 14:52:04 +0200

    Workaround for GitHub pages routing
    "GitHub Pages does not support routing for single page apps"

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/react_task_board'>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
