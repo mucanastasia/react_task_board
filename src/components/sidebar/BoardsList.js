50b9fe9627c5612bd647823d6ec48747c7b47cdb
mucanastasia <44086697+mucanastasia@users.noreply.github.com>
2024-06-09 18:48:03 +0200

    Updating Sidebar and BoardsList components

--- src/components/sidebar/BoardsList.js
+++ src/components/sidebar/BoardsList.js
@@ -1,40 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-export default function BoardsList({ isOpen }) {
-    const boardsList = [
-        {
-            name: 'Research Project Management',
-            id: '1',
-            active: true,
-            src: './board_1',
-        },
-        {
-            name: 'Task board 2',
-            id: '2',
-            active: false,
-            src: './board_2',
-        },
-        {
-            name: 'Task board 3',
-            id: '3',
-            active: false,
-            src: './board_3',
-        },
-    ];
+export default function BoardsList({ isOpen, boards }) {
 
     const renderBoardsList = () => {
-        const list = boardsList.map((board) => (
-            <NavLink to={board.src} activeclassname='active' key={board.id}>
+        const boardsList = boards.map((board, index) => (
+            <NavLink to={board.path} activeclassname='active' key={board.id}>
                 <li>
                     <span className='icon icon_board'></span>
-                    <span className='span_id'>{board.id}</span>
+                    <span className='span_id'>{index + 1}</span>
                     {!isOpen && <span className='tooltip'>{board.name}</span>}
                     <span className={`text ${isOpen ? 'visible' : ''}`}>{board.name}</span>
                 </li>
             </NavLink>
         ));
-        return list;
+        return boardsList;
     };
 
     return (
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,44 +1,53 @@
 import React, { useEffect, useState } from 'react';
+import { useBoard } from '../../contexts/BoardContext';
 import { getSidebarStatusFromLocalStorage, setSidebarStatusInLocalStorage } from '../../services/localStorageService';
 import { NavLink } from 'react-router-dom';
+import boardHelpers from '../../helpers/boardHelpers';
 import BoardsList from './BoardsList';
 import './sidebar.css';
 
 export default function Sidebar() {
+    const { boards, setBoards } = useBoard();
+    const { addBoard } = boardHelpers(boards, setBoards);
     const [isOpen, setIsOpen] = useState(getSidebarStatusFromLocalStorage);
 
     useEffect(() => {
         setSidebarStatusInLocalStorage(isOpen);
         //TODO: sidebarContext >> use isOpen to shrink a page
-        isOpen ? document.body.childNodes[3].childNodes[1].style.margin = '0 0 0 240px' : document.body.childNodes[3].childNodes[1].style.margin = '0 0 0 64px';
+        // isOpen ? document.body.childNodes[3].childNodes[1].style.margin = '0 0 0 240px' : document.body.childNodes[3].childNodes[1].style.margin = '0 0 0 64px';
+        isOpen ? document.body.style.margin = '0 0 0 240px' : document.body.style.margin = '0 0 0 64px';
     }, [isOpen]);
 
     const toggleSidebar = () => {
         setIsOpen(!isOpen);
     };
 
+    const handleClick = () => {
+        addBoard();
+    };
+
     return (
         <nav className={`sidebar ${isOpen ? 'open' : ''}`}>
             <ul>
                 <div className='upperNav'>
-                    <NavLink to='./' exact='true' activeclassname='active' >
+                    <NavLink to='/' exact='true' activeclassname='active' >
                         <li className='homeNav'>
                             <span className='icon icon_home'></span>
                             {!isOpen && <span className='tooltip'>Home</span>}
                             <span className={`text ${isOpen ? 'visible' : ''}`}>Home</span>
                         </li>
                     </NavLink>
-                    <NavLink to='./stats' activeclassname='active' >
+                    <NavLink to='/stats' activeclassname='active' >
                         <li>
                             <i className='icon icon_stats'></i>
                             {!isOpen && <span className='tooltip'>Statistics</span>}
                             <span className={`text ${isOpen ? 'visible' : ''}`}>Statistics</span>
                         </li>
                     </NavLink>
-                    <BoardsList isOpen={isOpen} />
+                    <BoardsList isOpen={isOpen} boards={boards} />
                 </div>
                 <div className='lowerNav'>
-                    <li>
+                    <li onClick={handleClick} >
                         <span className='icon icon_add_board'></span>
                         {!isOpen && <span className='tooltip'>Add a board</span>}
                         <span className={`text ${isOpen ? 'visible' : ''}`}>Add a board</span>
