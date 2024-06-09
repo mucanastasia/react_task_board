de978db419e9237ff923a2e37a1f6fe905cb6896
mucanastasia <44086697+mucanastasia@users.noreply.github.com>
2024-06-09 13:13:07 +0200

    Extracting PageTransition into separate component

--- src/App.js
+++ src/App.js
@@ -1,84 +1,39 @@
 import React from 'react';
-import { TasksProvider } from './contexts/TasksContext';
+import { useBoard } from './contexts/BoardContext';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { BoardNameProvider } from './contexts/BoardNameContext';
 import { Route, Routes, useLocation } from 'react-router-dom';
-import { AnimatePresence, motion } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
+import PageTransition from './components/pageTransition/PageTransition';
 import Sidebar from './components/sidebar/Sidebar';
 import Home from './components/home/Home';
 import Statistics from './components/statistics/Statistics';
 import TaskBoard from './components/taskBoard/TaskBoard';
 import './App.css';
 
-const pageVariants = {
-  initial: {
-    opacity: 0,
-  },
-  in: {
-    opacity: 1,
-    y: 0,
-  },
-  out: {
-    opacity: 0,
-  },
-};
-
-const pageTransition = {
-  duration: 0.25,
-};
-
-function App() {
+export default function App() {
   const location = useLocation();
+  const { boards } = useBoard();
+
+  const renderBoardsRoute = () => {
+    const list = boards.map(({ id, path, name }) => (<Route key={id} path={path} element={<PageTransition><TaskBoard boardId={id} /></PageTransition>} />));
+    return list;
+  }
 
   return (
     <ThemeProvider>
       <BoardNameProvider>
         <Sidebar />
-        <TasksProvider>
-
-          <div className='page'>
-            <AnimatePresence mode='wait'>
-              <Routes location={location} key={location.pathname}>
-                <Route path='/' element={
-                  <motion.div
-                    initial="initial"
-                    animate="in"
-                    exit="out"
-                    variants={pageVariants}
-                    transition={pageTransition}
-                  >
-                    <Home />
-                  </motion.div>
-                } />
-                <Route path='/stats' element={
-                  <motion.div
-                    initial="initial"
-                    animate="in"
-                    exit="out"
-                    variants={pageVariants}
-                    transition={pageTransition}
-                  >
-                    <Statistics />
-                  </motion.div>
-                } />
-                <Route path='/board_1' element={
-                  <motion.div
-                    initial="initial"
-                    animate="in"
-                    exit="out"
-                    variants={pageVariants}
-                    transition={pageTransition}
-                  >
-                    <TaskBoard />
-                  </motion.div>
-                } />
-              </Routes>
-            </AnimatePresence>
-          </div>
-        </TasksProvider>
+        <div className='page'>
+          <AnimatePresence mode='wait'>
+            <Routes location={location} key={location.pathname}>
+              <Route path='/' element={<PageTransition><Home /></PageTransition>} />
+              <Route path='/stats' element={<PageTransition><Statistics /></PageTransition>} />
+              {renderBoardsRoute()}
+            </Routes>
+          </AnimatePresence>
+        </div>
       </BoardNameProvider>
     </ThemeProvider>
   );
-}
-
-export default App;
+};
---
+++ src/components/pageTransition/PageTransition.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const pageVariants = {
+  initial: {
+    opacity: 0,
+  },
+  in: {
+    opacity: 1,
+    y: 0,
+  },
+  out: {
+    opacity: 0,
+  },
+};
+
+const pageTransition = {
+  duration: 0.25,
+};
+
+export default function PageTransition({ children }) {
+  return (
+    <motion.div
+      initial="initial"
+      animate="in"
+      exit="out"
+      variants={pageVariants}
+      transition={pageTransition}
+    >
+      {children}
+    </motion.div>
+  );
+};
