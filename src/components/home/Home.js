ee3c4201b2d2272ee487467bee76a358c87184c7
mucanastasia <44086697+mucanastasia@users.noreply.github.com>
2024-06-20 17:24:20 +0200

    Adding navigation features for home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,14 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useBoard } from '../../contexts/BoardContext';
+import { useTheme } from '../../contexts/ThemeContext';
+import boardHelpers from '../../helpers/boardHelpers';
+import { Link } from 'react-router-dom';
 import PageTransition from '../pageTransition/PageTransition';
 import './home.css';
 
 export default function Home() {
+    const [name, setName] = useState('');
+    const [loading, setLoading] = useState(false);
+    const { boards, setBoards } = useBoard();
+    const { addBoard } = boardHelpers(boards, setBoards);
+    const { theme } = useTheme();
+
+    useEffect(() => {
+        const h2Element = document.querySelector('.textContainer');
+        const h3Element = document.querySelector('.textCopyright');
+
+        const handleTransition = () => {
+            h2Element.classList.add('shrink-and-move-up');
+            h3Element.classList.add('move-down');
+        };
+
+        // Trigger the transition after the initial fade-in
+        setTimeout(handleTransition, 3500);
+    }, []);
+
+    const handleClick = () => {
+        setLoading(true);
+        addBoard(name);
+        setTimeout(() => {
+            setName('');
+            setLoading(false);
+        }, 700);
+    };
+
+    const handleChange = (e) => {
+        setName(e.target.value);
+    };
+
+    const renderBoardsList = () => {
+        const boardsList = boards.map((board, index) => (
+            <Link to={board.path} key={board.id}>
+                <div className='boardsListItem'>
+                    <span className='icon icon_board'></span>
+                    <span className='span_id'>{index + 1}</span>
+                    <p>{board.name}</p>
+                </div>
+            </Link>
+        ));
+        return boardsList;
+    };
+
+    //TODO: Divide into components
 
     return (
         <PageTransition className='wave-header' keyProp='home'>
             <div className='inner-header flex'>
                 <h2 className='textContainer'>Welcome to the Task Board Application</h2>
                 <h3 className='textCopyright'>© 2024 <a href='https://github.com/mucanastasia'>mucanastasia</a></h3>
+                <div className='navigation'>
+                    <div className={`newBoardSection ${theme}`}>
+                        <h5>Create a new board</h5>
+                        <input className={`boardName ${theme}`}
+                            type='text'
+                            value={name}
+                            onChange={handleChange}
+                            placeholder='Name your task board'
+                            autoFocus />
+                        <button className={`essential ${loading && 'loading'}`} onClick={handleClick} disabled={!name?.trim().length > 0 || loading}>
+                            {loading ? <span className='spinner'></span> : 'Confirm'}</button>
+                    </div>
+                    {boards.length > 0 && <div className='boardsList'>
+                        <h5>Navigate to the existing one</h5>
+                        {renderBoardsList()}
+                    </div>}
+                </div>
             </div>
 
             <div>
